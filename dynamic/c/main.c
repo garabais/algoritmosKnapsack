8587564cdf0bd6c7fcdc54b48fc987270035fe7e
Mauricio Xavier Garabito Ramirez <A01635930@itesm.mx>
2020-04-22 13:05:57 -0500

    change the type definition to be more clear

--- dynamic/c/main.c
+++ dynamic/c/main.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define lu unsigned long
+// Defining the data like this to make migration to unsigned long long easy if necesary
+#define number unsigned long
 
-lu max(lu a, lu b) { return (a > b)? a : b; }
+number max(number a, number b) { return (a > b)? a : b; }
 
-lu** knapSack(lu W, lu *wt, lu *val, lu n) {
-    lu i, w;
+number** knapSack(number W, number *wt, number *val, number n) {
+    number i, w;
 
-    lu **K = (lu**) malloc((n+1) * sizeof(long*));
-    K[0] = (lu *)calloc(W+1, sizeof(long));
+    number **K = (number**) malloc((n+1) * sizeof(long*));
+    K[0] = (number *)calloc(W+1, sizeof(long));
 
     for (i = 1; i <= n; i++) {
-        K[i] = (lu *)malloc((W+1) * sizeof(long));
+        K[i] = (number *)malloc((W+1) * sizeof(long));
         for (w = 0; w <= W; w++) {
             if (w==0){
                 K[i][w] = 0;
@@ -28,18 +29,18 @@ lu** knapSack(lu W, lu *wt, lu *val, lu n) {
     
 }
 
-void display(lu **arr, lu I, lu J) {
-    lu i, j;
+void display(number **arr, number I, number J) {
+    number i, j;
     for (i = 0; i <= I; i++) {
         for (j = 0; j <= J; j++) {
-            printf("%lu ", arr[i][j]);
+            printf("%number ", arr[i][j]);
         }
         printf("\n");
     }
 }
 
-void freeMatrix(lu **arr, lu I) {
-    lu i;
+void freeMatrix(number **arr, number I) {
+    number i;
     for (i = 0; i <= I; i++) {
         free(arr[i]);
     }
@@ -47,26 +48,26 @@ void freeMatrix(lu **arr, lu I) {
 }
 
 int main() {
-    lu i, n, W;
+    number i, n, W;
 
-    scanf("%lu", &n);
+    scanf("%number", &n);
 
-    lu *val = (lu*)malloc(n * sizeof(long)),
-        *wt  = (lu*)malloc(n * sizeof(long));
+    number *val = (number*)malloc(n * sizeof(long)),
+        *wt  = (number*)malloc(n * sizeof(long));
 
     for(i = 0;i < n; ++i){
-    	scanf("%lu", &val[i]);
+    	scanf("%number", &val[i]);
     }
 
     for(i = 0;i < n; ++i){
-    	scanf("%lu", &wt[i]);
+    	scanf("%number", &wt[i]);
     }
 
-    scanf("%lu", &W);
+    scanf("%number", &W);
 
-    lu **solution = knapSack(W, wt, val, n);
+    number **solution = knapSack(W, wt, val, n);
 
-    printf("%lu\n", solution[n][W]);
+    printf("%number\n", solution[n][W]);
 
     display(solution, n, W);
 
